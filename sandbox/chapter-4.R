3100a242b687835212d743bc273319844d7f1395
Joey Merrin <gjmerrin@gmail.com>
2019-01-24 15:25:30 -0500

    completed chapter 4 activities

--- sandbox/chapter-4.R
+++ sandbox/chapter-4.R
@@ -43,23 +43,40 @@ deck2 <- deck[1:52, ]
 head(deck2)
 deck3 <- deck[c(2, 1, 3:52), ]
 head(deck3)
+random <- sample(1:52, size = 52)
+random
+deck4 <- deck[random, ]
+head(deck4)
+# Exercise
+# card <- deck - # for testing and development
+shuffle <- function(cards) {
+  random <- sample(1:52, size = 52)
+  cards[random, ]
+}
+deal(deck)
+deck2 <- shuffle(deck)
+deal(deck2)
+# doallar signs and double brackets
+deck$value
+mean(deck$value)
+median(deck$value)
+lst <- list(numbers = c(1, 2), logical = TRUE, strings = c("a", "b", "c"))
+lst
+# reutns a smaller list
+lst[1]
+# returns selected values as they are, with no list structure
+lst$numbers
+sum(lst$numbers)
+# double brackets for position if name is not specifed returns values
+lst[[1]]
+# single brackets keeps the list structure
+lst[1]
+lst["numbers"]
+lst[["numbers"]]
+# Figure 4-3. It can be helpful to think of your list as a train. Use single brackets to select
+# train cars, double brackets to select the contents inside of a car.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# dont use attach() - it attaches the dataset so you can call variables without spelling it out like deck$value
 
 
 
